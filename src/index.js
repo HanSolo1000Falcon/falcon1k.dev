dc8bc5b1291b0484955dfa1e658e49abb9816506
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-02-27 17:25:01 +0100

    Yes, I learned very basic HTML for this.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import main from './main.html';
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+
+		if (!url.pathname || url.pathname === '/') {
+			return new Response(main, {
+				status: 200,
+				headers: {
+					'Content-Type': 'text/html'
+				}
+			})
+		}
 	},
 };
