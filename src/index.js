cf7360a152cb43de5448bebb553188ad46c4ce08
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-03-01 13:24:40 +0100

    Added Discord key.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
 
+		if (url.pathname === '/.well-known/discord') {
+			return new Response('dh=1ee52f325dede87e81931f9fe4b6fc5a7effa045', {
+				status: 200,
+				headers: {
+					'Content-Type': 'text/plain',
+				}
+			})
+		}
+
 		if (!url.pathname || url.pathname === '/') {
 			return new Response(main, {
 				status: 200,
